e7cecca973600a0c31fa58ff7915f613948a7bbe
Dindihub <sadindi03@gmail.com>
2022-06-27 10:35:38 +0300

    Add cloudinary to settings

--- mychakula/settings.py
+++ mychakula/settings.py
@@ -191,6 +191,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # adding config
 cloudinary.config(
+
   cloud_name="YOUR_CLOUD_NAME", 
   api_key="YOUR_API_KEY", 
   api_secret="YOUR_API_SECRET" 
